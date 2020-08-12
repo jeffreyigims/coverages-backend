f9d3953f69420c9d79c5462edf41ed68ba3e1307
Jeffrey Igims <jigims@andrew.cmu.edu>
2020-08-11 21:58:09 -0400

    Changed variable access method

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -9,7 +9,6 @@ Knock.setup do |config|
   ## Default:
   # config.token_lifetime = 1.day
 
-
   ## Audience claim
   ## --------------
   ##
@@ -41,7 +40,7 @@ Knock.setup do |config|
   ## If using Auth0, uncomment the line below
   # config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
   # config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
-  config.token_secret_signature_key = -> { Rails.application.fetch(:secret_key_base) }
+  config.token_secret_signature_key = -> { Rails.application.secret_key_base }
 
   ## Public key
   ## ----------
